5f891e62e899fa48e882a7ac13a9baa9bbc2fc09
saodilong2 <173569172@qq.com>
2016-03-23 11:26:10 +0800

    mybatis demo

---
+++ mybatis-demo/README.md
@@ -0,0 +1,7 @@
+# mybatis-demo
+
+Spring boot with MyBatis
+
+has convert to kotlin version
+
+please see two tag of : java-version and kotlin-pure
---
+++ mybatis-demo/src/main/kotlin/com/example/domain/City.kt
@@ -0,0 +1,9 @@
+package com.example.domain
+
+import java.io.Serializable
+
+/**
+ * Created by qiuzhanghua on 16/2/24.
+ */
+data class City(var id: Long = 0L, var name:String = "", var state: String = "", var country: String = "")
+
